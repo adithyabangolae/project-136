6482603a0bc2329053ca1d5d98f12bda5e45fcdc
adithyabangolae <71918422+adithyabangolae@users.noreply.github.com>
2022-03-05 15:48:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, jsonify, request
+from main_data import data
+
+#df = pd.DataFrame(data, columns=['name', 'mass', 'radius', 'distance','gravity'])
+
+
+app = Flask(__name__)
+@app.route("/")
+
+def index():
+    return jsonify({
+        "data":data,
+        "message":"success"
+        
+    }),200
+    
+@app.route("/star")
+
+def planet():
+    name = request.args.get("name")
+    star_data = next(item for item in data if item[name] == name )
+    return jsonify({
+        "data": star_data,
+        "message": "success"
+       
+    }),200
+    
+if __name__ == "__main__":
+    app.run()
